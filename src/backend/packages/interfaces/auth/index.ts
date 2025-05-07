6d2547e88a6a68a546cdbaa5e3e6a9529554f4d1
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-07 09:57:59 -0300

    File: src/backend/packages/interfaces/auth/index.ts

---
+++ src/backend/packages/interfaces/auth/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @file Central export file for all authentication interfaces
+ * @module @austa/interfaces/auth
+ */
+
+// User interfaces
+export * from './user.interface';
+
+// Permission interfaces
+export * from './permission.interface';
+
+// Role interfaces
+export * from './role.interface';
+
+// Authentication interfaces
+export * from './auth.interface';
+
+// Request/Response interfaces
+export * from './request.interface';
+export * from './response.interface';
