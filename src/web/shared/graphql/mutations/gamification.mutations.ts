f56a387c76f1deb3439add73dde00181db3e6e39
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-19 10:06:23 -0300

    File: src/web/shared/graphql/mutations/gamification.mutations.ts

--- src/web/shared/graphql/mutations/gamification.mutations.ts
+++ src/web/shared/graphql/mutations/gamification.mutations.ts
@@ -1,1 +1,52 @@
-Failed to process file
+import { gql } from '@apollo/client'; // v3.8.10
+
+/**
+ * Mutation to claim a reward for a user
+ */
+export const CLAIM_REWARD = gql`
+  mutation ClaimReward($rewardId: ID!) {
+    claimReward(rewardId: $rewardId) {
+      id
+      title
+      description
+      icon
+      type
+      value
+    }
+  }
+`;
+
+/**
+ * Mutation to complete a task within a quest for a user
+ */
+export const COMPLETE_QUEST_TASK = gql`
+  mutation CompleteQuestTask($questId: ID!, $taskId: ID!) {
+    completeQuestTask(questId: $questId, taskId: $taskId) {
+      id
+      title
+      description
+      journey
+      progress
+      total
+      completed
+    }
+  }
+`;
+
+/**
+ * Mutation to acknowledge an achievement by a user
+ */
+export const ACKNOWLEDGE_ACHIEVEMENT = gql`
+  mutation AcknowledgeAchievement($achievementId: ID!) {
+    acknowledgeAchievement(achievementId: $achievementId) {
+      id
+      title
+      description
+      icon
+      progress
+      total
+      unlocked
+      journey
+    }
+  }
+`;
