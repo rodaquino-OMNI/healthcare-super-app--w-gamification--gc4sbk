1582a476fb4d7ad7082ea9eecda688dd425d21ac
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-19 10:07:29 -0300

    File: src/web/shared/graphql/mutations/care.mutations.ts

--- src/web/shared/graphql/mutations/care.mutations.ts
+++ src/web/shared/graphql/mutations/care.mutations.ts
@@ -1,1 +1,37 @@
-Failed to process file
+import { gql } from '@apollo/client'; // @apollo/client version 3.8.10
+import { CARE_PROVIDER_UNAVAILABLE, CARE_APPOINTMENT_SLOT_TAKEN, CARE_TELEMEDICINE_CONNECTION_FAILED } from '@austa/interfaces/common/error-codes';
+import { Appointment } from '@austa/interfaces/care';
+
+/**
+ * GraphQL mutation to book an appointment.
+ * 
+ * This mutation allows users to schedule an appointment with a healthcare provider
+ * as part of the Care Now journey. It requires provider ID, date/time, and appointment type,
+ * with an optional reason field.
+ */
+export const BOOK_APPOINTMENT = gql`
+    mutation BookAppointment($providerId: String!, $dateTime: String!, $type: String!, $reason: String) {
+        bookAppointment(providerId: $providerId, dateTime: $dateTime, type: $type, reason: $reason) {
+            id
+            providerId
+            dateTime
+            type
+            reason
+        }
+    }
+`;
+
+/**
+ * GraphQL mutation to cancel an appointment.
+ * 
+ * This mutation allows users to cancel a previously booked appointment
+ * by providing the appointment ID. It returns the ID of the cancelled appointment
+ * to confirm the operation was successful.
+ */
+export const CANCEL_APPOINTMENT = gql`
+    mutation CancelAppointment($id: ID!) {
+        cancelAppointment(id: $id) {
+            id
+        }
+    }
+`;
