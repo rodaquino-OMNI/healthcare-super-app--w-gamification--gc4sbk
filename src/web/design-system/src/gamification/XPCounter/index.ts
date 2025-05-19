9078b04c570e73f788cc564d6d39488646cd548d
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-19 02:35:21 -0300

    File: src/web/design-system/src/gamification/XPCounter/index.ts

---
+++ src/web/design-system/src/gamification/XPCounter/index.ts
@@ -0,0 +1,6 @@
+import { XPCounterProps } from '@austa/interfaces/gamification';
+import { XPCounter, calculateRemainingXP, calculateProgress } from './XPCounter';
+
+export { XPCounter, calculateRemainingXP, calculateProgress };
+export type { XPCounterProps };
+export default XPCounter;
