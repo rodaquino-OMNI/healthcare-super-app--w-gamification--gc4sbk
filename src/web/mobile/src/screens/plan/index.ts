96654380f22d798e31d4060f4908b68ea21412aa
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-19 07:43:53 -0300

    File: src/web/mobile/src/screens/plan/index.ts

--- src/web/mobile/src/screens/plan/index.ts
+++ src/web/mobile/src/screens/plan/index.ts
@@ -1,1 +1,19 @@
-Failed to process file
+import { Coverage } from './Coverage';
+import { DigitalCardScreen } from './DigitalCard';
+import { ClaimSubmissionScreen } from './ClaimSubmission';
+import ClaimHistory from './ClaimHistory';
+import { CostSimulatorScreen } from './CostSimulator';
+import PlanDashboard from './Dashboard';
+import { ClaimDetail } from './ClaimDetail';
+import BenefitsScreen from './Benefits';
+
+export {
+    Coverage,
+    DigitalCardScreen as DigitalCard,
+    ClaimSubmissionScreen,
+    ClaimHistory,
+    CostSimulatorScreen,
+    PlanDashboard,
+    ClaimDetail,
+    BenefitsScreen
+};
