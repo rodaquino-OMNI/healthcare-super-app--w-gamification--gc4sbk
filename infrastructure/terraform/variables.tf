c6741d7c5d39aa2688d8f0005e82f42990c55dcf
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-19 15:26:52 -0300

    File: infrastructure/terraform/variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,1 +1,106 @@
-Failed to process file
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy resources"
+  default     = "sa-east-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., staging, production)"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Name of the project"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDR blocks for public subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDR blocks for private subnets"
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+variable "database_name" {
+  type        = string
+  description = "Name of the PostgreSQL database"
+  default     = "austa"
+}
+
+variable "database_username" {
+  type        = string
+  description = "Username for the PostgreSQL database"
+  sensitive   = true
+}
+
+variable "database_password" {
+  type        = string
+  description = "Password for the PostgreSQL database"
+  sensitive   = true
+}
+
+variable "database_instance_class" {
+  type        = string
+  description = "Instance class for the PostgreSQL database"
+  default     = "db.m5.large"
+}
+
+variable "redis_node_type" {
+  type        = string
+  description = "Instance type for the Redis cluster"
+  default     = "cache.m5.large"
+}
+
+variable "redis_num_cache_nodes" {
+  type        = number
+  description = "Number of cache nodes in the Redis cluster"
+  default     = 3
+}
+
+variable "kafka_brokers" {
+  type        = number
+  description = "Number of Kafka brokers"
+  default     = 3
+}
+
+variable "certificate_arn" {
+  type        = string
+  description = "ARN of the ACM certificate for the load balancer"
+  default     = ""
+}
+
+# ECR Repository Configuration
+variable "ecr_repository_names" {
+  type        = list(string)
+  description = "List of ECR repository names for frontend packages"
+  default     = ["design-system", "primitives", "interfaces", "journey-context"]
+}
+
+variable "ecr_lifecycle_policy_days" {
+  type        = number
+  description = "Number of days to retain untagged images in ECR repositories"
+  default     = 30
+}
+
+variable "ecr_developer_access_roles" {
+  type        = list(string)
+  description = "List of IAM roles that should have developer access to the ECR repositories"
+  default     = []
+}
+
+variable "ecr_ci_cd_access_roles" {
+  type        = list(string)
+  description = "List of IAM roles that should have CI/CD pipeline access to the ECR repositories"
+  default     = []
+}
