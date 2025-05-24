5640a6d5c3601b9a5da56197384d589ec4b665ce
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-24 13:34:44 -0300

    File: src/web/design-system/src/plan/index.ts

--- src/web/design-system/src/plan/index.ts
+++ src/web/design-system/src/plan/index.ts
@@ -4,16 +4,14 @@
  * providing a centralized access point for Plan-related UI elements.
  * 
  * This simplifies imports for consumers of the design system:
- * import { BenefitCard, ClaimCard, CoverageInfoCard, InsuranceCard } from '@austa/design-system/plan';
+ * import { BenefitCard, ClaimCard } from '@austa/design-system/plan';
  */
 
-// Import components from their respective folders
 import { BenefitCard } from './BenefitCard/BenefitCard';
 import { ClaimCard } from './ClaimCard/ClaimCard';
 import { CoverageInfoCard } from './CoverageInfoCard/CoverageInfoCard';
 import { InsuranceCard } from './InsuranceCard/InsuranceCard';
 
-// Re-export all Plan journey components
 export {
   BenefitCard,
   ClaimCard,
