b2e4bffa279aac336749ccfa7cc9b10453674cd5
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-05-24 15:33:52 -0300

    File: src/web/design-system/src/health/GoalCard/index.ts

---
+++ src/web/design-system/src/health/GoalCard/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @file index.ts
+ * @description Barrel file for the GoalCard component
+ */
+
+import GoalCard from './GoalCard';
+export type { GoalCardProps } from './GoalCard';
+
+export default GoalCard;
